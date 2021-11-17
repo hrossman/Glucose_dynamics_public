135290b8ab5eadd389cbecdd34b9c9d52dc9813a
hagair <hagairossman@gmail.com>
2021-11-17 15:15:07 +0200

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,8 @@ import streamlit as st
 def Ra_func_gaussian(t: float, H: float, T: float, W: float) -> float:
     return (H*np.exp(-((t-T)**2)/W))
 
-
-def GOM_ODE(t, ys, Ra, Gb, theta1, theta2, theta3):
-    G, X = ys
-    dG = -G*X - theta3*(G-Gb) + Ra(t)
-    dX = -theta1*X + theta2*(G-Gb)
-    return [dG, dX]
-
-
-def run_ode_sim(params, t_start, t_end, t_points=1000):
+def Ra_double_gaussian(t, params):
     # Unpack model params
-    Gb = params['Gb']
-    G0 = params['G0']
-    X0 = params['X0']
-    theta1 = params['theta1']
-    theta2 = params['theta2']
-    theta3 = params['theta3']
     H1 = params['H1']
     T1 = params['T1']
     W1 = params['W1']
@@ -32,27 +18,85 @@ def run_ode_sim(params, t_start, t_end, t_points=1000):
     T2 = params['T2']
     W2 = params['W2']
 
-    y0 = [G0, X0]
-    t_span = [t_start, t_end]
-    t_eval = np.linspace(t_start, t_end, t_points)
-
     # Compute Ra input
-    Ra_value = Ra_func_gaussian(t_eval, H1, T1, W1) + \
-        Ra_func_gaussian(t_eval, H2, T2, W2)
-    Ra = interp1d(x=t_eval, y=Ra_value, bounds_error=False)
+    Ra_value = Ra_func_gaussian(t, H1, T1, W1) + \
+        Ra_func_gaussian(t, H2, T2, W2)
+    Ra = interp1d(x=t, y=Ra_value, bounds_error=False)
+    return Ra
 
-    # Solve ODE
-    sol = solve_ivp(GOM_ODE, t_span=t_span, y0=y0, t_eval=t_eval,
-                    args=(Ra, Gb, theta1, theta2, theta3), dense_output=True)
 
-    # Output to pd Dataframe
-    t = t_eval
-    z = sol.sol(t).T
-    result_df = pd.DataFrame(z, columns=['G', 'X'])
-    result_df['Ra'] = Ra(t)
-    result_df['t'] = t
+def GOM_ODE_func(t, ys, Ra, params):
+    Gb = params['Gb']
+    theta1 = params['theta1']
+    theta2 = params['theta2']
+    theta3 = params['theta3']
+    G, X = ys
+    dG = -G*X - theta3*(G-Gb) + Ra(t)
+    dX = -theta1*X + theta2*(G-Gb)
+    return [dG, dX]
 
-    return result_df
+
+#-- ODE class
+class Glucose_ODE:
+    def __init__(self) -> None:
+        self.name = 'GOM'
+        self.title = 'Glucose Only Model (GOM)'
+        self.equations = r"""
+        dG/dt = -G(t)X(t)-\theta_3\left(G(t)-G_b\right) \\
+        dX/dt = -\theta_1X(t) +\theta_2X\left(G(t)-G_b\right) \\
+        Ra(t) = H_1exp\left(\frac{-(t-T_1)^2}{W_1}\right)
+        """
+        # model params
+        t_mult = 0.01
+        self.params = {
+            'theta1':1.5*t_mult,
+            'theta2':0.5*t_mult**2,
+            'theta3':0.1*t_mult,
+            'G0': 5.7,
+            'X0': 0,
+            'Gb': 5.7,
+            'H1':0.5,
+            'T1':100,
+            'W1':300, 
+            'H2':0.05,
+            'T2':100,
+            'W2':120
+        }
+
+        # inital conditions vec
+        self.y0 = [self.params['G0'], self.params['X0']]
+
+        # simulation params
+        self.t_start = 0
+        self.t_end = 700
+        self.t_points = 1000
+        self.t_span = [self.t_start, self.t_end]
+        self.t_eval = np.linspace(self.t_start, self.t_end, self.t_points)
+
+        self.ode_func = GOM_ODE_func
+        self.Ra_func = Ra_double_gaussian 
+
+        # # Compute dynamics upon init
+        # self.simulate() 
+
+
+    def run_ode_sim(self):    
+        # Solve ODE
+        sol = solve_ivp(self.ode_func, t_span=self.t_span, y0=self.y0, t_eval=self.t_eval,
+                        args=(self.Ra, self.params), dense_output=True)
+        # Output to pd Dataframe
+        z = sol.sol(self.t_eval).T
+        dynamics_df = pd.DataFrame(z, columns=['G', 'X'])
+        dynamics_df['Ra'] = self.Ra(self.t_eval)
+        dynamics_df['t'] = self.t_eval
+        return dynamics_df
+
+
+    def simulate(self):
+        # Compute Ra input
+        self.Ra = self.Ra_func(self.t_eval, self.params)
+        # Solve dynamics
+        self.dynamics_df = self.run_ode_sim()
 
 
 #-- STREAMLIT
@@ -115,33 +159,32 @@ with col2:
 
 
 with col3:
-    # simulation params
-    t_start = 0
-    t_end = 700
-    t_points = 1000
-    t = np.linspace(t_start, t_end, t_points)
+    ## SIMULATE
+    model = Glucose_ODE()
+    model.params = params
+    model.simulate()
+    dynamics_df = model.dynamics_df
+    t = model.t_eval
 
-    df = run_ode_sim(params, t_start, t_end, t_points)
-    # st.write(df)
+    # st.write(dynamics_df)
+
+    # Title & Equation
+    st.header(model.title)
+    st.latex(model.equations)
+
     # Plot outputs
     fig, axes = plt.subplots(3, 1)
-
     ax = axes[0]
-    ax.plot(t, df['Ra'])
+    ax.plot(t, dynamics_df['Ra'])
     ax.set(ylabel='Ra(t)', xlabel='t')
     ax.grid()
-
     ax = axes[1]
-    ax.plot(t, df['G'])
+    ax.plot(t, dynamics_df['G'])
     ax.set(ylabel='G(t)', xlabel='t')
     ax.grid()
-
     ax = axes[2]
-    ax.plot(t, df['X'])
+    ax.plot(t, dynamics_df['X'])
     ax.set(ylabel='X(t)', xlabel='t')
     ax.grid()
-
     fig.tight_layout()
     st.pyplot(fig)
-
-    

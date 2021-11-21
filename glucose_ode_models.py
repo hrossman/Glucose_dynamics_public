bc8e4301f5b5b8446742993b666908d3e6192a71
hagair <hagairossman@gmail.com>
2021-11-21 17:09:52 +0200

    New model now working

--- glucose_ode_models.py
+++ glucose_ode_models.py
@@ -43,12 +43,11 @@ class Glucose_ODE(object):
         self.Ra_func = None
 
         # simulation params
-        self.t_start = 0
-        self.t_end = 700
+        self.t_start = 5
+        self.t_end = 60*10
         self.t_points = 1000
         self.t_span = [self.t_start, self.t_end]
         self.t_eval = np.linspace(self.t_start, self.t_end, self.t_points)
-        
 
     def run_ode_sim(self):
         # Solve ODE
@@ -91,15 +90,15 @@ class Glucose_ODE(object):
 
 
 #-- Specific model classes
-class GOM_ODE(Glucose_ODE):
+class GOM_ODE_OLD(Glucose_ODE):
     def __init__(self) -> None:
         super().__init__()
         self.name = 'GOM'
         self.title = 'Glucose Only Model (GOM)'
         self.equations = r"""
-        dG/dt = -G(t)X(t)-\theta_3\left(G(t)-G_b\right) \\
+        dG/dt = -G(t)X(t)-\theta_3\left(G(t)-G_b\right) + Ra(t) \\
         dX/dt = -\theta_1X(t) +\theta_2X\left(G(t)-G_b\right) \\
-        Ra(t) = H_1exp\left(\frac{-(t-T_1)^2}{W_1}\right) \\
+        Ra(t) = H_1exp\left(\frac{-(t-T_1)^2}{W_1}\right)
                 + H_2exp\left(\frac{-(t-T_2)^2}{W_2}\right)
         """
         self.dynamic_vars = ['G', 'X']
@@ -155,3 +154,188 @@ class GOM_ODE(Glucose_ODE):
         self.Ra_func = Ra_double_gaussian
 
         
+class GOM_ODE(Glucose_ODE):
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = 'GOM'
+        self.title = 'Glucose Only Model (GOM)'
+        self.equations = r"""
+        dG/dt = -G(t)X(t)-p_1[G(t)-G_b] \\
+                + Ra(t)/V \\
+        dX/dt = -p_2[X(t)-S_GZ(t)] \\
+        Z(t) = \frac{G(t)-G_b}{1+exp[-\alpha(G(t)-G_b)]} + \beta Ra/V
+        """
+        self.dynamic_vars = ['G', 'X']
+        self.dynamic_vars_labels = ['Glucose values', 'X values']
+        self.dynamic_vars_colors = [COLOR_G, COLOR_X]
+        # model params
+        self.params = {
+            'p1': 25e-3, #'[1/min]',
+            'p2': 20e-3, #'[1/min]',
+            'SG': 10e-4, # '[10^(-4)1/min per mg/dL]
+            'alpha': 0.1, #'[dL/mg]'
+            'beta': 5, #'[min]'
+            'T1': 20, #'[min]'
+            'T2': 120, #'[min]'
+            'W1': 0.5,
+            'W2': 0.5,
+            'RH': 0.8,
+            'V': 1.45, #'[dL/kg]',
+            'lambda': 0,
+            'f': 0.9,
+            # 'bodyweight_kg': 75,
+            # 'input_mg': 75_000,
+            'D': 75_000/75, #'[mg/kg]',
+            'A': 0.9*(75_000/75), #'[mg/kg]',
+            'Gb': 90, #'[mg/dL]',
+            'G0': 91, #'[mg/dL]',
+            'X0': 0, #'[1/min]',
+        }
+
+        #### TODO ####
+        # Calculated params
+        # self.calced_params = ['D', 'A']
+        # self.params['D'] = self.params['input_mg']/self.params['bodyweight_kg'] 
+        # self.params['A'] = self.params['D']*self.params['f']
+        
+        
+        # step sizes for streamlit
+        self.params_steps = {
+            'p1': 0.01, #'[1/min]',
+            'p2': 0.01, #'[1/min]',
+            'SG': 2, # '[10^(-4)1/min per mg/dL]
+            'alpha': 0.1, #'[dL/mg]'
+            'beta': 1, #'[min]'
+            'T1': 10, #'[min]'
+            'T2': 20, #'[min]'
+            'W1': 0.2,
+            'W2': 0.2,
+            'RH': 0.2,
+            'V': 1, #'[dL/kg]',
+            'lambda': 0.1,
+            'f': 0.1,
+            # 'bodyweight_kg': 10,
+            # 'input_mg': 10_000,
+            'D': 100, #'[mg/kg]',
+            'A': 100, #'[mg/kg]',
+            'Gb': 90, #'[mg/dL]',
+            'G0': 90, #'[mg/dL]',
+            'X0': 0, #'[1/min]',
+        }
+        # inital conditions
+        self.y0 = [self.params['G0'], self.params['X0']]
+
+        # ode function
+        def ODE_func(t, ys, Ra, params):
+            Gb = params['Gb']
+            p1 = params['p1']
+            alpha = params['alpha']
+            beta = params['beta']
+            p2 = params['p2']
+            SG = params['SG']
+            V = params['V']
+            G, X = ys
+
+            dG = -G*X - p1*(G-Gb) + Ra(t)/V
+            Zpos = 1+np.exp(-alpha*(G-Gb))
+            Z = (G-Gb)/Zpos + beta*Ra(t)/V
+            dX = -p2*(X-SG*Z)
+            return [dG, dX]
+        self.ode_func = ODE_func
+      
+        # input function
+        def Ra_Func(t, params):
+            # Unpack model params
+            A = params['A']
+            RH = params['RH']
+            T1 = params['T1']
+            W1 = params['W1']
+            T2 = params['T2']
+            W2 = params['W2']
+
+            def f_LN(t, T, W):
+                return (np.exp(-((np.log(t/T)-W/2)**2)/W))/(t*np.sqrt(np.pi*W))
+            Ra_value = A*(1-RH)*f_LN(t, T1, W1) + A*(RH)*f_LN(t, T2, W2)
+            Ra_value = Ra_value
+            Ra = interp1d(x=t, y=Ra_value, bounds_error=False)
+            return Ra
+        self.Ra_func = Ra_Func
+
+    def calc_aux_vars(self):
+        if self.dynamic_vars is None:
+            self.simlulate()
+
+        Gb = self.params['Gb']
+        alpha = self.params['alpha']
+        beta = self.params['beta']
+        SG = self.params['SG']
+        V = self.params['V']
+        G = self.dynamics_df['G']
+        X = self.dynamics_df['X']
+        Ra = self.Ra
+        t = self.t_eval
+
+        G_minus_Gb = G-Gb
+        Zpos = 1 + np.exp(-alpha*(G-Gb))
+        Z = (G-Gb)/Zpos + beta*Ra(t)/V
+        Y_GOM = SG*Z
+
+        aux_vars_df = pd.DataFrame({'t':t, 'G_minus_Gb':G_minus_Gb, 'Zpos':Zpos, 'Y_GOM':Y_GOM})
+        self.aux_vars_df = aux_vars_df
+        self.aux_vars = aux_vars_df.drop('t', axis=1).columns
+
+    def plot_aux_vars(self):
+        # Plot outputs
+        n_vars = len(self.aux_vars)+1
+        fig, axes = plt.subplots(n_vars, 1, figsize=(6, 0.6+n_vars), sharex=True)
+        # Plot Ra input
+        ax = axes[0]
+        ax.plot(self.t_eval, self.dynamics_df['Ra']/self.params['V'],
+                lw=1, color=COLOR_RA, alpha=0.5, label='Ra/V')
+        ax.fill_between(
+            self.t_eval, 0, self.dynamics_df['Ra']/self.params['V'], color=COLOR_RA, alpha=0.4)
+        ax.set(ylabel='Ra(t)/V')
+        ax.grid()
+        # plot aux vars
+        for i, var in enumerate(self.aux_vars):
+            ax = axes[i+1]
+            ax.plot(self.t_eval, self.aux_vars_df[var], ls='-', lw=4,
+            alpha=0.5, label=var)
+            ax.set(ylabel=f'{var}(t)')
+            ax.grid()
+        fig.tight_layout()
+        return fig, axes
+
+
+if __name__ == "__main__":
+    model = GOM_ODE()
+
+    # model.params = {
+    #         'p1': 25e-3, #'[1/min]',
+    #         'p2': 20e-3, #'[1/min]',
+    #         'SG': 10e-4, # '[10^(-4)1/min per mg/dL]
+    #         'alpha': 0.1, #'[dL/mg]'
+    #         'beta': 5, #'[min]'
+    #         'T1': 20, #'[min]'
+    #         'T2': 120, #'[min]'
+    #         'W1': 0.5,
+    #         'W2': 0.5,
+    #         'RH': 0.8,
+    #         'V': 1.45, #'[dL/kg]',
+    #         'lambda': 0,
+    #         'f': 0.9,
+    #         'D': 75_000/75, #'[mg/kg]',
+    #         'A': None, #'[mg/kg]',
+    #         'Gb': 90, #'[mg/dL]',
+    #         'G0': 91, #'[mg/dL]',
+    #         'X0': 10, #'[1/min]',
+    #     }
+    # model.params['A'] = model.params['D']*model.params['f']
+    model.simulate()
+    # fig, axes = model.plot_dynamic_vars()
+    # plt.show()
+    print(model.calc_aux_vars())
+    fig, axes = model.plot_aux_vars()
+    fig, axes = model.plot_dynamic_vars()
+
+    plt.show()
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from config import COLOR_G, COLOR_X, COLOR_RA
-from glucose_ode_models import GOM_ODE
+from glucose_ode_models import GOM_ODE, GOM_ODE_OLD
 
 #-- STREAMLIT
 # Init model
 model = GOM_ODE()
+# model = GOM_ODE_OLD()
 
-param_min_val = -2000.
-param_max_val = 2000.
+
+param_min_val = -99999.
+param_max_val = 99999.
 new_params = {}
 
 st.set_page_config(layout="wide")
+
 with st.sidebar:
-    # Title & Equation
     st.header(model.title)
-    st.latex(model.equations)
     # params inputs
     for param_name, param_default_val in model.params.items():
         param_step = model.params_steps[param_name]
@@ -30,24 +31,9 @@ with st.sidebar:
 model.params = new_params
 model.simulate()
 dynamics_df = model.dynamics_df
-t = model.t_eval
-
-# # Plot outputs
-# fig, axes = plt.subplots(3, 1, figsize=(6,4), sharex=True)
-# ax = axes[0]
-# ax.plot(t, dynamics_df['Ra'], lw=1, color=COLOR_RA, alpha=0.5, label='Glucose values')
-# ax.fill_between(t, 0, dynamics_df['Ra'], color=COLOR_RA, alpha=0.4)
-# ax.set(ylabel='Ra(t)')
-# ax.grid()
-# ax = axes[1]
-# ax.plot(t, dynamics_df['G'], ls='-', lw=4, color=COLOR_G, alpha=0.5, label='Glucose values')
-# ax.set(ylabel='G(t)')
-# ax.grid()
-# ax = axes[2]
-# ax.plot(t, dynamics_df['X'], lw=4, color=COLOR_X, alpha=0.5, label='Glucose values')
-# ax.set(ylabel='X(t)', xlabel='t')
-# ax.grid()
-# fig.tight_layout()
-# st.write(print((model.dynamic_vars)))
 fig, axes = model.plot_dynamic_vars()
 st.pyplot(fig)
+
+# Title & Equation
+st.header(model.title)
+st.latex(model.equations)
